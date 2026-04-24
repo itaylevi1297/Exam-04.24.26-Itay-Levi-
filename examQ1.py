8027801a972c870890946018d79f693763b9b548
itaylevi1297 <itaylevi1297@gmail.com>
2026-04-24 12:58:25 +0300

    Add files via upload

---
+++ examQ1.py
@@ -0,0 +1,29 @@
+Ranks = []
+
+while True:
+    user_input = input("enter rank:")
+
+    try:
+        rank = int(user_input)
+    except ValueError:
+        continue
+
+    if rank == -999:
+        if len(Ranks) >= 10:
+            break
+        else:
+            continue
+
+    if 1 <= rank <= 5:
+        Ranks.append(rank)
+
+
+avg_rank = sum(Ranks) / len(Ranks)
+max_rank = max(Ranks)
+valid_count = len(Ranks)
+
+print("avg rank:", avg_rank)
+print("max rank:", max_rank)
+print("valid count:", valid_count)
+
+
